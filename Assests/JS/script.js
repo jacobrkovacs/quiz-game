58a82d1dd62b2c5c78e975d8a0c93c3173cababe
Jacob Kovacs <jwkovacs.19@gmail.com>
2023-07-10 14:47:20 -0700

    mod css and js files

--- Assests/JS/script.js
+++ Assests/JS/script.js
@@ -7,10 +7,13 @@ var questionNumEl = document.querySelector(".heading")
 var questionEl = document.querySelector(".questions")
 var answerEl = document.querySelectorAll(".answer")
 
+var timer = null;
+var timeRemaining = 0;
+
 var correct = 0;
 var wrong = 0;
-var currentQuestionIndex = [];
-var currentAnswerIndex = [];
+var currentQuestionIndex;
+var currentAnswerIndex;
 var currentGuess = [];
 
 
@@ -20,51 +23,51 @@ const questions = [
     "What do you use to iterate through an array?",
     "Which of the following is a data type?",
     "Inside which HTML element do we link an external JavaScript file?",
-    "What is the proper syntax for setting an attribute to an element using JavaScript?",
+    "What is the proper method for setting an attribute to an element using JavaScript?",
     "What does console.log('Hello World') do?",
 ];
 
-const answers = [
-    ["Style", "Structure", "Interactivity", "Interdimensional Travel"],
-    ["An 'if' statement", "A 'for' loop", "iterate.array", ".querySelector"],
-    ["", "", "", ""],
-    ["", "", "", ""],
-    ["", "", "", ""],
-    ["", "", "", ""]
+const answers = {
+    q1: ["Style", "Structure", "Interactivity", "Interdimensional Travel"],
+    q2: ["An 'if' statement", "A 'for' loop", "iterate.array", ".querySelector"],
+    q3: ["String", "Function", "Data", "Interval"],
+    q4: ["<head>", "<body>", "<knees>", "<footer>"],
+    q5: [".attSetting()", ".setting.Att()", ".attributeSet()", ".setAttribute()"],
+    q6: ["Logs 'Hello World' In the consile", "Sends a messge to the world", "Saves 'Hello World' to the internet", "Syntax Error"]
+}
+
+const correctAnswers = [
+    answers.q1[2],
+    answers.q2[1],
+    answers.q3[0],
+    answers.q4[1],
+    answers.q5[3],
+    answers.q6[0]
 ]
-var highscores = []
-var playerName = [];
 
+const duration = 21;
 
+var highscores = {}
 
 function init() {
     var scores = JSON.parse(localStorage.getItem(highscores));
-    var player = JSON.parse(localStorage.getItem(playerName));
 
     if(scores) {
         scores = highscores.scores
     }
 
-    if(player){
-        player = playerName.player
-    }
-
-
 }
 
 function clickStart(event){
-    countdown();
-
-    for(let i = 0; i < questions.length; i++){
+    
+    if (!timer) {
+        timeRemaining = duration
+        timer = setInterval(countdown, 1000)
         currentQuestionIndex = Math.floor(Math.random() * questions.length)
-        questionEl.textContent = questions[currentQuestionIndex];
-        
-        questionNumEl.textContent = "Question #" + (i + 1) + ":";
-
-        if(questions[currentQuestionIndex]){
-            currentAnswerIndex = answers[i];
-            answerEl.innerHTML = answers.currentAnswerIndex
-        }
+        questionEl.textContent = questions[currentQuestionIndex]
+        startButtonEl.setAttribute("style", "display: none;")
+        introEl.setAttribute("style", "display: none;")
+        scoreButtonEl.setAttribute("style", "display: none;")
     }
     
 
@@ -74,9 +77,11 @@ function clickStart(event){
 
     //hide intro screen
 
-    //hide start button
+    //hide start button 
 
     //hide results
+
+
 
 
 }
@@ -84,20 +89,17 @@ startButtonEl.addEventListener("click", clickStart);
 
 
 function countdown(){
-    var timer = 21;
-    var timerCountdown = setInterval(function(){
-        timer--;
-        timeRemainingEl.textContent = timer;
-        if(timer <= 10){
-            timeRemainingEl.setAttribute("style", "color: red")
-        }
-        if(timer < 0) {
-            clearInterval(timerCountdown)
-            timeRemainingEl.setAttribute("style", "display: none;")
-            alert("TIME'S UP!")
-        }
-    }, 1000);
-}
+    timeRemaining--;
+    timeRemainingEl.textContent = timeRemaining;
+    if(timeRemaining<= 10){
+        timeRemainingEl.setAttribute("style", "color: red")
+    }
+    if(timeRemaining < 0) {
+        clearInterval(timer)
+        timeRemainingEl.setAttribute("style", "display: none;")
+        alert("TIME'S UP!")
+    }
+};
 
 function questionAnswered(){
 
@@ -110,3 +112,13 @@ function gameEnd() {
 function storeScores() {
 
 }
+
+function hideElement(element){
+    element.classList.add("hide")
+}
+
+function hideElement(element){
+    element.classList.remove("hide")
+}
+
+init();
