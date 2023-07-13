571a7446f9d727cf0fe2e82454d87f4998681b23
Jacob Kovacs <jwkovacs.19@gmail.com>
2023-07-12 18:08:23 -0700

    mod html, css and js

--- Assests/JS/script.js
+++ Assests/JS/script.js
@@ -1,22 +1,27 @@
 
 var startButtonEl = document.querySelector("#begin")
 var timeRemainingEl = document.querySelector("#timer")
-var introEl = document.querySelector("#intro")
-var scoreButtonEl = document.querySelector("#scoreboard")
-var questionNumEl = document.querySelector(".heading")
+var quizEl = document.querySelector(".quiz")
+var introEl = document.querySelector(".intro")
+var challengeEl = document.querySelector(".challenge")
+var instructionEl = document.querySelector(".instructions")
+var scoreButtonEl = document.querySelector(".scoreboard")
+var headingEl = document.querySelector(".heading")
 var questionEl = document.querySelector(".questions")
-var answerEl = document.querySelectorAll(".answer")
+var answerEl = document.getElementById("answers")
+var answerBtnEl = document.getElementsByClassName(".answer")
+var initialInput = document.querySelector(".score")
+var finalScore = document.getElementById("score")
+var submitBtn = document.getElementById("submit")
+var input = document.getElementById("input")
 
 var timer = null;
 var timeRemaining = 0;
 
-var correct = 0;
-var wrong = 0;
+var score = 0;
+var newScore = input.value + ": " + score;
 var currentQuestionIndex;
 var currentAnswerIndex;
-var currentGuess = [];
-
-
 
 const questions = [
     "What is JavaScript mainly used for?",
@@ -27,98 +32,148 @@ const questions = [
     "What does console.log('Hello World') do?",
 ];
 
-const answers = {
-    q1: ["Style", "Structure", "Interactivity", "Interdimensional Travel"],
-    q2: ["An 'if' statement", "A 'for' loop", "iterate.array", ".querySelector"],
-    q3: ["String", "Function", "Data", "Interval"],
-    q4: ["<head>", "<body>", "<knees>", "<footer>"],
-    q5: [".attSetting()", ".setting.Att()", ".attributeSet()", ".setAttribute()"],
-    q6: ["Logs 'Hello World' In the consile", "Sends a messge to the world", "Saves 'Hello World' to the internet", "Syntax Error"]
-}
+const answers = [
+    ["Style", "Structure", "Interactivity", "Interdimensional Travel"],
+    ["An 'if' statement", "A 'for' loop", "iterate.array", ".querySelector"],
+    ["String", "Function", "Data", "Interval"],
+    ["<head>", "<body>", "<knees>", "<footer>"],
+    [".attSetting()", ".setting.Att()", ".attributeSet()", ".setAttribute()"],
+    ["Logs 'Hello World' In the console", "Sends a messge to the world", "Saves 'Hello World' to the internet", "Syntax Error"]
+]
 
 const correctAnswers = [
-    answers.q1[2],
-    answers.q2[1],
-    answers.q3[0],
-    answers.q4[1],
-    answers.q5[3],
-    answers.q6[0]
+    answers[0][2],
+    answers[1][1],
+    answers[2][0],
+    answers[3][1],
+    answers[4][3],
+    answers[5][0]
 ]
 
 const duration = 21;
 
-var highscores = {}
-
 function init() {
     var scores = JSON.parse(localStorage.getItem(highscores));
 
-    if(scores) {
-        scores = highscores.scores
-    }
-
 }
 
-function clickStart(event){
+function clickStart(){
     
     if (!timer) {
         timeRemaining = duration
         timer = setInterval(countdown, 1000)
-        currentQuestionIndex = Math.floor(Math.random() * questions.length)
-        questionEl.textContent = questions[currentQuestionIndex]
-        startButtonEl.setAttribute("style", "display: none;")
-        introEl.setAttribute("style", "display: none;")
-        scoreButtonEl.setAttribute("style", "display: none;")
+        //hide intro elements
+        hideElement(instructionEl)
+        hideElement(scoreButtonEl)
+        hideElement(startButtonEl)
+        hideElement(challengeEl)
+        showElement(quizEl)
+        score = 0
     }
-    
-
-    //choose question
-
-    //questionAnswered();
-
-    //hide intro screen
-
-    //hide start button 
-
-    //hide results
-
-
-
-
 }
-startButtonEl.addEventListener("click", clickStart);
-
 
 function countdown(){
     timeRemaining--;
     timeRemainingEl.textContent = timeRemaining;
-    if(timeRemaining<= 10){
+    if(timeRemaining <= 10){
         timeRemainingEl.setAttribute("style", "color: red")
     }
-    if(timeRemaining < 0) {
-        clearInterval(timer)
-        timeRemainingEl.setAttribute("style", "display: none;")
-        alert("TIME'S UP!")
-    }
+    handleGameEnd();
 };
 
+var q = 1;
+var i = 0;
+
+function populateAnswers(){
+    handleGameEnd();
+    //set question number
+    headingEl.textContent = "Question #" + (q) + ":"
+    //choose question
+    questionEl.innerText = questions[i]
+    for (let j = 0; j < 4; j++){
+        answerEl.children[j].textContent = answers[i][j]
+    }
+}
+
+var clickedCorrect;
 function questionAnswered(){
-
+    clickedCorrect = correctAnswers[i]
+    answerEl.addEventListener("click", function(event){
+        event.stopPropagation();
+        if(event.target.innerText == clickedCorrect){
+            event.target.classList.add("correct");
+            setTimeout(populateAnswers, 750);
+            score = score + 5;
+            console.log(score)
+            setTimeout(cleanup, 750);
+            handleGameEnd();
+        }else{
+            event.target.classList.add("wrong");
+            setTimeout(populateAnswers, 750);
+            // timeRemaining = timeRemaining - 3;
+            setTimeout(cleanup, 750);
+            handleGameEnd();
+        }
+    })
 }
 
-function gameEnd() {
-
+function cleanup(){
+    for(let i = 0; i < 4; i++){
+        answerEl.children[i].classList.remove("correct")
+        answerEl.children[i].classList.remove("wrong")
+    }
+    answerEl.classList.remove("wrong")
+    clickedCorrect = correctAnswers[i];
+    i++
+    q++
 }
 
-function storeScores() {
+function showScoreboard() {
+    hideElement(startButtonEl)
+    hideElement(instructionEl)
+    hideElement(challengeEl)
+}
 
+function storeScore(){
+    localStorage.setItem("scores", score)
 }
 
 function hideElement(element){
     element.classList.add("hide")
 }
 
-function hideElement(element){
+function showElement(element){
     element.classList.remove("hide")
 }
 
+function handleGameEnd(){
+    if(i >= questions.length + 1) {
+        hideElement(quizEl)
+        clearInterval(timer)
+        alert("GAME OVER!")
+        hideElement(timeRemainingEl)
+        showElement(startButtonEl)
+        showElement(scoreButtonEl)
+        timer = null;
+        timeRemaining = 0;
+        q = 1
+        i = 0
+        headingEl.textContent = "Question #" + (q) + ":"
+        questionEl.innerText = questions[i]
+        for (let j = 0; j < 4; j++){
+            answerEl.children[j].textContent = answers[i][j]
+        }
+        clickedCorrect = correctAnswers[i];
+        showElement(initialInput)
+
+        return
+
+    }   
+}
+
 init();
+populateAnswers();
+questionAnswered();
+startButtonEl.addEventListener("click", clickStart);
+scoreButtonEl.addEventListener("click", showScoreboard)
+submitBtn.addEventListener("click", storeScore)
